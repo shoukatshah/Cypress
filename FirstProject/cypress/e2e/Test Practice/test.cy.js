a19d0a4fd9e7b94d2e8290414e464cdcbf03fca4
shoukatshah <m.shoukat@devbatch.com>
2023-08-01 15:09:01 +0500

    fixtures

--- FirstProject/cypress/e2e/Test Practice/test.cy.js
+++ FirstProject/cypress/e2e/Test Practice/test.cy.js
@@ -1,14 +1,21 @@
 
 describe('Files', () => {
-    it("this is my first test case for halaltable ",() => {
-        cy.visit('https://beta.halaltable.online/')
-        cy.wait(5000)
-        cy.get("#header-login-btn").click({force:true})
-        cy.fixture('credentials.json').then((credentials) =>{
-                cy.get('#email-login').type(credentials.email)
-                cy.get('#pass-login').type(credentials.password)   
+        it("this is my first test case for halaltable ", () => {
+                cy.visit('https://beta.halaltable.online/')
+                cy.wait(5000)
+                cy.get("#header-login-btn").click({ force: true })
+                cy.fixture('credentials.json').then((credentials) => {
+                        let findQuery = (x) => x.id = 3;
+
+                        let credential = credentials.find(x => findQuery(x));
+
+                        console.log("User2 Email: ", credential.email)
+                        console.log("User2 Password: ", credential.password)
+
+                        cy.get('#email-login').type(credential.email)
+                        cy.get('#pass-login').type(credential.password)
+
+                })
 
         })
-              
-      })         
 })
